378ac3b9bd001051faff0604392ebc5e90627e03
Naaadddiii <95406520+Naaadddiii@users.noreply.github.com>
2024-12-15 01:50:40 +0530

    Add CORS middleware to resolve CORS errors

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/tasks");
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 connectDB();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
