e14cb31917821672b35d44ec44a8ba94357cc6b0
Naaadddiii <95406520+Naaadddiii@users.noreply.github.com>
2024-12-16 16:44:15 +0530

    Fix CORS configuration in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(
   cors({
     origin:
-      "https://task-management-frontend-ovuxnktky-padmanaban-vs-projects.vercel.app",
+      "https://task-management-frontend-git-main-padmanaban-vs-projects.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
